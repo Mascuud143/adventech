61249928e96661ac844ffb827836f8a6ab9140ae
mascuud <mascuud61@gmail.com>
2021-11-15 13:02:37 +0100

    init

--- routes/calender.Router.js
+++ routes/calender.Router.js
@@ -6,4 +6,5 @@ const Router = express.Router();
 
 Router.get("/", Controllers.getCalenders);
 Router.post("/", Auth.protect, Controllers.createCalender);
+Router.post("/g", Auth.protect, Controllers.createCalender);
 module.exports = Router;
